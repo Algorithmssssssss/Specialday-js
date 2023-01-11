03e06f73c0ff0e1135b86933682e89a4f1430eed
Algorithmsssss <reachkim.uni@gmail.com>
2023-01-11 11:37:15 +0200

    move apps to heart.jsx

--- specialday-js/src/App.jsx
+++ specialday-js/src/App.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useWindupString, WindupChildren } from "windups";
-
+import { useWindupString, WindupChildren, Pace, CharWrapper } from "windups";
 
 export default function App() {
   return (
@@ -11,17 +10,21 @@ export default function App() {
   );
 }
 
+const myComponentStyle = {
+  backgroundColor: 'pink',
+  fontFamily: 'Comic Sans MS',
+  height: '500px',
+
+
+}
+
 const Wishes = () => {
   const [text] = useWindupString(
     "Happy Birthday, my love! I hope you have a wonderful day",
     {
-      pace: (char) => (char === " " ? 300 : 40),
+      pace: (char) => (char === " " ? 500 : 40),
     }
   );
-
-
-
-  
 
   React.useEffect(() => {
     const el = document.getElementById("container");
@@ -33,12 +36,14 @@ const Wishes = () => {
   return (
     <>
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" style={myComponentStyle}>
           <h1>{text}</h1>
           <WindupChildren>
             {"Hi my "}
             <span style={{ color: "red" }}>{"love "}</span>
             {"I love you"}
+            <Pace ms={100}>{" G-R-O-U-N-D-E-D."}</Pace>
+
           </WindupChildren>
         </div>
       </div>
